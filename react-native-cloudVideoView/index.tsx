596cc5814d8614efe2a2d3b8c01cc979eff48cde
jianglingzhi <38064420@qq.com>
2022-05-25 11:04:58 +0800

    no message

--- react-native-cloudVideoView/index.tsx
+++ react-native-cloudVideoView/index.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import { requireNativeComponent, findNodeHandle, UIManager, ViewStyle } from 'react-native'
 
 const RNCloudVideo = requireNativeComponent('RNCloudVideoView')
+
+interface videoProgress {
+  duration: number
+  progress: number
+  total: number
+}
 interface RNCloudVideoViewProps {
-  repeat: Boolean
-  mode: 'cover' | 'contain'
-  bearing: 'vertical' | 'transverse'
-  cache: number
-  rate: number
-  onPlayProgress: (event: any) => {}
-  onPlayState: (event: any) => {}
+  repeat?: Boolean
+  mode?: 'cover' | 'contain'
+  bearing?: 'vertical' | 'transverse'
+  cache?: number
+  rate?: number
+  onPlayProgress?: (event: videoProgress) => {}
+  onPlayState?: (state: number) => {}
   src: string
-  paused: Boolean
+  paused?: Boolean
   style: ViewStyle
 }
 export default class RNCloudVideoView extends React.Component<RNCloudVideoViewProps, any> {
@@ -28,26 +34,26 @@ export default class RNCloudVideoView extends React.Component<RNCloudVideoViewPr
   }
   //调节播放进度
   seek = (nubmer: number) => {
-    // UIManager.dispatchViewManagerCommand(findNodeHandle(this.refs.RNCloudVideo), UIManager.RNCloudVideoView.Commands.seek, [nubmer])
+    UIManager.dispatchViewManagerCommand(findNodeHandle(this.refs.RNCloudVideo), UIManager.RNCloudVideoView.Commands.seek, [nubmer])
   }
   //暂停播放
   pause = () => {
-    // UIManager.dispatchViewManagerCommand(findNodeHandle(this.refs.RNCloudVideo), UIManager.RNCloudVideoView.Commands.pause, [])
+    UIManager.dispatchViewManagerCommand(findNodeHandle(this.refs.RNCloudVideo), UIManager.RNCloudVideoView.Commands.pause, [])
   }
   //继续播放
   resume = () => {
-    // UIManager.dispatchViewManagerCommand(findNodeHandle(this.refs.RNCloudVideo), UIManager.RNCloudVideoView.Commands.resume, [])
+    UIManager.dispatchViewManagerCommand(findNodeHandle(this.refs.RNCloudVideo), UIManager.RNCloudVideoView.Commands.resume, [])
   }
   render() {
-    const { onPlayProgress, onPlayState, preload, ...other } = this.props
+    const { onPlayProgress, onPlayState, ...other } = this.props
     return (
       <RNCloudVideo
         ref="RNCloudVideo"
         onPlayProgress={event => {
-          onPlayProgress(event.nativeEvent)
+          onPlayProgress && onPlayProgress(event.nativeEvent)
         }}
         onPlayState={event => {
-          onPlayState(event.nativeEvent)
+          onPlayState && onPlayState(event.nativeEvent)
         }}
         {...other}
       />
